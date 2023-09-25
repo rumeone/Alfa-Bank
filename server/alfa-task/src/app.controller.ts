eaac9f9ba14d26cb8b0a40bad704c43b2c59ad34
rumeone <yuaroslav_29@mail.ru>
2023-09-26 01:13:04 +0700

    Made a simple route for uploading a file

--- server/alfa-task/src/app.controller.ts
+++ server/alfa-task/src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/getData')
+  async getData(@Res() response: Response) {
+    const filePath = path.join('./resources/testdata.json');
+
+    try {
+      const data = await fs.promises.readFile(filePath, 'utf-8');
+      const jsonData = JSON.parse(data);
+      response.json(jsonData);
+    } catch (e) {
+      console.log(e);
+      throw new HttpException(
+        'Не удалось считать файл!',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
